976b183f5f3babe59a518192f8c37108d2845848
Aditya <adityasinghak17@gmail.com>
2025-12-12 23:03:28 +0530

    is_prime function

---
+++ prime.py
@@ -0,0 +1,12 @@
+from math import *
+def is_prime(n):
+    num = n
+    if num==1:
+        return False
+    for i in range(2,int(sqrt(num))+1):
+        if num%i==0:
+            return False
+    return True
+
+print(is_prime(7))
+
