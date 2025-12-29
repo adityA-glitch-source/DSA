2ea1db9fd19bad83ab305bc7b13d60d14c33096e
Aditya <adityasinghak17@gmail.com>
2025-12-29 12:12:17 +0530

    Checks the array is sorted or not

--- check_if_the array_is_sorted_or_Not.py
+++ check_if_the array_is_sorted_or_Not.py
@@ -1,13 +1,9 @@
 def check(nums):
     n = nums
-    flag = False
     for i in range(0,len(n)-1):
         if n[i]>n[i+1]:
-            flag = True
-            break
-    if flag == False:
-        return True
-    else:
-        return False
+            return False
+
+    return True
 
 print(check([1,2,5,8,3,10,14,15]))
