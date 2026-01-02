95fd926300e943e10bff544a89e2b38f09c7050e
Aditya <adityasinghak17@gmail.com>
2026-01-02 19:46:54 +0530

    Rotates a list by k places using slicing

--- Right rotate a array by k places.py
+++ Right rotate a array by k places.py
@@ -1,9 +1,17 @@
+#def rotate(nums,k):
+    #n = len(nums)
+    #rotations = k%n
+    #for i in range(0,rotations):
+        #e = nums.pop()
+        #nums.insert(0,e)
+#nums = [3,9,5,6,7,2]
+#rotate(nums,3)
+#print(nums)
+
+## Better
 def rotate(nums,k):
-    n = len(nums)
-    rotations = k%n
-    for i in range(0,rotations):
-        e = nums.pop()
-        nums.insert(0,e)
-nums = [3,9,5,6,7,2]
-rotate(nums,3)
-print(nums)
+     n = len(nums)
+     k = k%n
+     nums[:] = nums[k:] + nums[0:k]
+     return nums
+print(rotate([3,9,5,6,7,2],3))
