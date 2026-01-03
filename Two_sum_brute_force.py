7572662082f220c18b79d8ce4ed633c4ab8a101e
Aditya <adityasinghak17@gmail.com>
2026-01-03 22:50:52 +0530

    solves two sum problem using brute force approach

---
+++ Two_sum_brute_force.py
@@ -0,0 +1,10 @@
+def two_sum(nums,target):
+    arr = nums
+    n = len(arr)
+    t = target
+    for i in range(0,n-1):
+        for j in range(i+1,n):
+            if arr[i] + arr[j] == t:
+                return [i,j]
+
+print(two_sum([5,9,1,2,4,15,6,3],13))
