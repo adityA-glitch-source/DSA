6baf38f119a95b634d37379bb1873b45f8c16a1f
Aditya <adityasinghak17@gmail.com>
2026-01-04 23:11:13 +0530

    maximum_sum_in_subarray

---
+++ maximum_sum_in_subarray.py
@@ -0,0 +1,13 @@
+def max_subarray(nums):
+    l = nums
+    n = len(nums)
+    maxi = float("-inf")
+    for i in range(0,n):
+        total = 0
+        for j in range(i,n):
+            total += l[j]
+            if total>maxi:
+               maxi = total
+    return maxi
+print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+
