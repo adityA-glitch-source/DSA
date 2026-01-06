621b93d57f274277747598b60cdad86c11b5a769
Aditya <adityasinghak17@gmail.com>
2026-01-06 23:32:38 +0530

    two_sum_new

--- Remove duplicate elements from sorted array.py
+++ Remove duplicate elements from sorted array.py
@@ -1,15 +1,30 @@
+#nums = [1,1,1,2,3,4,4,7,9,9,9,10]
+#n = len(nums)
+#if n == 1 :
+    #print(1)
+#else:
+    #i = 0
+    #j = i+1
+    #while j<n:
+       #if nums[i]!=nums[j]:
+            #i+=1
+            #nums[i],nums[j] = nums[j],nums[i]
+        #j+=1
+    #print(i+1)
 nums = [1,1,1,2,3,4,4,7,9,9,9,10]
 n = len(nums)
-if n == 1 :
+if nums == 1:
     print(1)
-else:
-    i = 0
-    j = i+1
-    while j<n:
-        if nums[i]!=nums[j]:
-            i+=1
-            nums[i],nums[j] = nums[j],nums[i]
-        j+=1
-    print(i+1)
+i = 0
+j = i +1
+while j<n:
+    if nums[i]!= nums[j]:
+        i+=1
+        nums[i],nums[j] = nums[j],nums[i]
+    j+=1
+print(i+1)
+
+
+
 
 
--- Two_sum_brute_force.py
+++ Two_sum_brute_force.py
@@ -8,5 +8,19 @@
 
 #print(two_sum([5,9,1,2,4,15,6,3],13))
 
+def two_sum(nums,target):
+    arr = nums
+    t = target
+    n = len(arr)
+    d = {}
+    for i in range(0,n):
+        remaining = target - arr[i]
+        if remaining in d:
+            return [i,d[remaining]]
+        else:
+            d[arr[i]] = i
+
+print(two_sum([5,9,1,2,4,15,6,3],13))
+
 
 
