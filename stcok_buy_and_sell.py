daf540086b13f44a197a43eff4bef5b286f2ac1c
Aditya <adityasinghak17@gmail.com>
2026-01-11 23:52:25 +0530

    solves stock buy and sell problem optimally in O(1) time complexity

--- stcok_buy_and_sell.py
+++ stcok_buy_and_sell.py
@@ -1,14 +1,32 @@
+#def stock(nums):
+    #arr = nums
+    #n = len(arr)
+    #maxi = 0
+    #total = 0
+    #for i in range(0,n):
+        #for j in range(i+1,n):
+            #if arr[j]>arr[i]:
+                #total = arr[j] - arr[i]
+            #maxi = max(maxi,total)
+    #return maxi
+
+#print(stock([7,2,1,5,6,4,8]))
+
+## Optimal solution
 def stock(nums):
     arr = nums
     n = len(arr)
-    maxi = 0
-    total = 0
+    mini = float("inf")
+    max_profit = 0
     for i in range(0,n):
-        for j in range(i+1,n):
-            if arr[j]>arr[i]:
-                total = arr[j] - arr[i]
-            maxi = max(maxi,total)
-    return maxi
+        if arr[i]<mini:
+            mini = arr[i]
+        else:
+            total = arr[i] - mini
+            max_profit = max(total,max_profit)
+    return max_profit
+
 
 print(stock([7,2,1,5,6,4,8]))
+
 
