a621028e82a823ab64740b4f3983fb097ae0a910
Aditya <adityasinghak17@gmail.com>
2026-01-17 17:53:31 +0530

    finds the upper triangle of a 2D list

---
+++ 2D_Array.py
@@ -0,0 +1,13 @@
+nums = [[5,20,3],[7,-10,9],[1,-52,6]]
+rows = len(nums)
+cols = len(nums[0])
+
+## Upper triangle
+for i in range(0,rows):
+    for j in range(0,cols):
+        if j>=i:
+            print(nums[i][j],end=" ")
+        else:
+            print('*',end=" ")
+
+    print()
